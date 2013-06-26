eab84d4a258ffbceae7bcbccd06a3796886283ac
stewart fraser <toepoke@hotmail.com>
2013-06-26 18:02:48 +0100

    Initial commit

---
+++ Git6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitWorld00");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
